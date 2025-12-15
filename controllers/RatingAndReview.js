e392dc12c14c01dbef00809b92360efa8f3a152b
Anand Raj Singh <singhanand31019@gmail.com>
2025-12-15 08:49:27 +0530

    add the rest of the controllers

--- controllers/RatingAndReview.js
+++ controllers/RatingAndReview.js
@@ -76,12 +76,30 @@ exports.getAverageRating = async (req,res) => {
             {
                 $group:{
                     _id:null,
-                    averageRating:{ $avg: "$rating"},
-                    
+                        averageRating:{ 
+                            $avg: "$rating"
+                        }                    
                 }
             }
         ])
          
+        if(result.length>0){
+            return res.status(200).json({
+                success:true,
+                averageRating:result[0].averageRating,
+            })
+        }
+
+
+
+        // if no rating review exist
+        return res.status(200).json({
+            success:true,
+            message:"Average rating is 0 , no rating is given till now",
+            averageRating:0
+        })
+
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
@@ -92,3 +110,32 @@ exports.getAverageRating = async (req,res) => {
 }
 //getAllRating
 
+exports.getAllRatingAndReviews = async(req,res)=>{
+    try {
+        const allReviews = await ratingAndReviews.find({})
+                                                 .sort({rating:"desc"})
+                                                 .populate({
+                                                    path:"user",
+                                                    select:"firstName lastName email image"
+                                                 })
+                                                 .populate({
+                                                    path:"course",
+                                                    select:"courseName"
+                                                 })
+                                                 .exec();
+
+
+    return res.status(200).json({
+        success:true,
+        message:"All reviews fetched successfully",
+        data:allReviews
+    })
+    } catch (error) {
+        return res.status().json({
+            success:false,
+            message:"failed to fetched"
+        })
+    }
+}
+
+// get reviews based on course
